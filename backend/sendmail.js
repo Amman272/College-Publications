95de3344763e641a735d8ea4254f6e0be80a6218
Amman272 <121443329+Amman272@users.noreply.github.com>
2025-12-17 14:04:40 +0530

    fixed email4

--- backend/sendmail.js
+++ backend/sendmail.js
@@ -2,7 +2,9 @@ import nodemailer from "nodemailer";
 
 export async function sendEmail(to,subject,html){
     const transporter= nodemailer.createTransport({
-        service:"gmail",
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false, // true for 465, false for other ports
         auth:{
             user: process.env.EMAIL_USER,
             pass:process.env.EMAIL_PASSWORD,
